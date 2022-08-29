66f81051aa6a43abca08606da32ddd4a4df4a736
Riyad <74069878+Riyad-boop@users.noreply.github.com>
2022-08-29 17:57:11 +0100

    split docker-compose into test and production

--- routers/celerytasks.py
+++ routers/celerytasks.py
@@ -1,6 +1,5 @@
 # dependancies:
-# import datetime as dt  # TODO REMOVE
-# from typing import Tuple  # TODO REMOVE
+import time
 
 # from tasks import scheduled_upsert_sensorSummary
 import tasks as CeleryWorker
@@ -8,7 +7,7 @@ import tasks as CeleryWorker
 # from tasks import add, read_sensor_data, scheduled_upsert_sensorSummary
 # error handling
 from celery.result import AsyncResult
-from fastapi import APIRouter, Body, HTTPException, Query, status
+from fastapi import APIRouter, HTTPException, Query, status
 from sqlalchemy.exc import IntegrityError
 
 celeryTasksRouter = APIRouter()
@@ -17,19 +16,20 @@ celeryTasksRouter = APIRouter()
 dateRegex = "\s+(?:0[1-9]|[12][0-9]|3[01])[-/.](?:0[1-9]|1[012])[-/.](?:19\d{2}|20\d{2}|2100)\b"
 
 
-# {"x": 100, "y": 123}
-@celeryTasksRouter.post("/ex1")
-def run_task(data=Body(...)):
-    x = data["x"]
-    y = data["y"]
-    task = CeleryWorker.add.delay(x, y)
-    return {"result": task.get()}
-
-
 @celeryTasksRouter.get("/task/{task_id}")
-def get_task_result(task_id: str):
-    res = AsyncResult(task_id)
-    return {"task_id": task_id, "task_status": res.status, "task_result": res.result}
+def get_task_result(task_id: str, timeoutSeconds: int = Query(default=60)):
+
+    for i in range(0, timeoutSeconds * 2):
+        res = AsyncResult(task_id)
+        if res.state == "SUCCESS":
+            return {"task_id": task_id, "task_status": res.status, "task_result": res.result}
+        elif res.state == "FAILURE":
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=res.result)
+
+        # sleep for half a second and then check again
+        time.sleep(0.5)
+
+    return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Task not found or timed out")
 
 
 # TODO validate query params here rather than in celery task
@@ -60,7 +60,8 @@ def get_sensorSummaries(
     # route task to celery
     task = CeleryWorker.read_sensor_data.delay(start, end, raw_data, geom_type, geom, sensor_ids)
 
-    return {"task_id": task.id}
+    return get_task_result(task.id, 60)
+    # return {"task_id": task.id}
 
 
 # # helper functions
